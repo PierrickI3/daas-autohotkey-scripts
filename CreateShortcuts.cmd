4a593118bd72636b802a1fcebdad4466200c4b1e
Pierrick Lozach <PierrickI3@users.noreply.github.com>
2015-08-18 11:09:18 +0200

    Create CreateShortcuts.cmd

---
+++ CreateShortcuts.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+
+REM "Create Loopback Line" shortcut
+REM -------------------------------
+echo sLinkFile = "%USERPROFILE%\Desktop\Create Loopback Line.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\vagrant\Desktop\Scripts\IA\Create Loopback Line.ahk" >> %SCRIPT%
+echo oLink.WorkingDirectory = "C:\Users\vagrant\Desktop\Scripts\IA" >> %SCRIPT%
+echo oLink.Description = "Creates a loopback line in Interaction Administrator" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
